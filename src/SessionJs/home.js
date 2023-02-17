ab57ae1b939421d7b30b0b6d771c12f1bd2f5ea0
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2023-02-17 20:33:32 +0530

    Update home.js

--- src/SessionJs/home.js
+++ src/SessionJs/home.js
@@ -4,8 +4,9 @@ import axios from "../axios";
 export default function Home() {
  
   const logOut = (event) => {
-    axios.get(`http://localhost:5000/api/logout`)
-      .then(result => {
+//     axios.get(`http://localhost:5000/api/logout`)
+   axios.get(`https://sessionlogin.onrender.com/api/logout`)
+          .then(result => {
         window.sessionStorage.clear();
         window.localStorage.clear();
         window.location.href = "/";
